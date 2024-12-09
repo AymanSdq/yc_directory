6a193807242b1632e5f30cc940893d4c754fa35d
AymanSdq <sedqiayman02@gmail.com>
2024-12-09 15:17:37 +0100

    All startup card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,22 @@
 import Image from "next/image";
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({searchParams} : {searchParams : Promise<{query? : string}>}) {
   
   const query = (await searchParams).query;
+
+  const posts = [{
+    _createdAt : new Date(),
+    views : 53,
+    author : {_id : 1, name : "Ayman Sedqi"},
+    _id : 1,
+    description : "This is a description",
+    image : "https://images.unsplash.com/photo-1487017159836-4e23ece2e4cf?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category : "Labtop",
+    title : "New Device"
+
+  }]
   
   return (
     <>
@@ -14,7 +27,19 @@ export default async function Home({searchParams} : {searchParams : Promise<{que
         <SearchForm query={query} />
       </section>
 
-      
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : 'All Startups'}
+
+          <ul className="mt-7 card_grid">
+            {posts?.length > 0 ? (
+              posts.map((post : StartupCardType, index: number) => (
+                <StartupCard key={post?._id} post={post}  />
+              ))
+            ) : (<p className="no-reesults">No Startups found!</p>)}
+          </ul>
+        </p>
+      </section>
     </>
   );
 }
